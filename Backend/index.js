da4107f9266208024fbf2b3fe62806a0c49a73d3
inyindjo1 <inyindjo1@gmail.com>
2025-07-15 16:03:46 -0400

    Removed and add more code in backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = 8080;
 
-
+app.use(cors());
 app.use(express.json());
 
 let jobs = [
