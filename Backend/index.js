486fc36623f21548fa9fbfa941fec66db4c1d0d2
inyindjo1 <inyindjo1@gmail.com>
2025-07-15 16:05:43 -0400

    remove prompt and replace to port

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,6 @@ import express from 'express'
 const app =express()
 const port = 8080
 app.get('/',(req,res) =>{
-    res.json('Job Finder(from server)')
+    res.send('Job Finder(from server)')
 })
-app.listen(port, () => console.log('Listening on:' + port))
+app.listen(prompt, () => console.log('Listening on:' + port))
